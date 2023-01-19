d48245fa523b894b899d761747bd0fb988633cbd
rasm445f <90387951+rasm445f@users.noreply.github.com>
2023-01-19 01:33:50 +0100

    made some tweaks and clean up, more to come

---
+++ src/test/java/facades/RentalFacadeTest.java
@@ -0,0 +1,90 @@
+package facades;
+
+import dtos.RentalDto;
+import dtos.RentalDto;
+import dtos.UserDTO;
+import entities.House;
+import entities.Tenant;
+import entities.Rental;
+import entities.User;
+import errorhandling.API_Exception;
+import javassist.NotFoundException;
+import org.junit.jupiter.api.*;
+import security.errorhandling.AuthenticationException;
+import utils.EMF_Creator;
+
+import javax.persistence.EntityManager;
+import javax.persistence.EntityManagerFactory;
+import java.util.ArrayList;
+import java.util.List;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertThrows;
+
+public class RentalFacadeTest {
+
+    private static EntityManagerFactory emf;
+    private static RentalFacade facade;
+    Rental rental1;
+    Rental rental2;
+
+    List<Tenant> tenants;
+    House house;
+    @BeforeAll
+    public static void setUpClass() {
+        emf = EMF_Creator.createEntityManagerFactoryForTest();
+        facade = RentalFacade.getRentalFacade(emf);
+    }
+
+    @AfterAll
+    public static void tearDownClass() {
+//        Clean up database after test is done or use a persistence unit with drop-and-create to start up clean on every test
+    }
+
+    @BeforeEach
+    void setUp() {
+        EntityManager em = emf.createEntityManager();
+        try {
+            em.getTransaction().begin();
+            em.createNamedQuery("Rental.deleteAllRows").executeUpdate();
+            tenants = new ArrayList<>();
+            house = new House(1,"address2","testby2",30);
+            rental1 = new Rental("2222-02-02","2222-22-22",2222,2222,house);
+            rental2 = new Rental("1111-11-11","1111-11-11",1111,1111,house);
+            em.persist(rental1);
+            em.persist(rental2);
+            em.getTransaction().commit();
+        } finally {
+            em.close();
+        }
+    }
+
+    @AfterEach
+    void tearDown() {
+
+    }
+
+    @Test
+    void getAllRentals() throws NotFoundException {
+        List<RentalDto> actual = facade.getAllRentals();
+        int expected = 2;
+        assertEquals(expected,actual.size());
+    }
+
+    @Test
+    void createRental() throws NotFoundException {
+        Rental rental = new Rental("2222-02-02","2222-22-22",2222,2222,house);
+        facade.createRental(rental);
+        List<RentalDto> actual = facade.getAllRentals();
+        int expected = 3;
+        assertEquals(expected,actual.size());
+    }
+
+    @Test
+    void deleteRental() throws NotFoundException, API_Exception {
+        facade.deleteRental(rental1.getId());
+        List<RentalDto> actual = facade.getAllRentals();
+        int expected = 1;
+        assertEquals(expected,actual.size());
+    }
+}
---
+++ src/test/java/rest/RentalResourceTest.java
@@ -0,0 +1,198 @@
+package rest;
+
+import com.google.gson.Gson;
+import com.google.gson.GsonBuilder;
+import dtos.RentalDto;
+import entities.House;
+import entities.Tenant;
+import entities.Rental;
+import entities.User;
+import io.restassured.RestAssured;
+import io.restassured.http.ContentType;
+import io.restassured.parsing.Parser;
+import io.restassured.path.json.JsonPath;
+import io.restassured.response.Response;
+import io.restassured.response.ResponseBody;
+import org.glassfish.grizzly.http.server.HttpServer;
+import org.glassfish.grizzly.http.util.HttpStatus;
+import org.glassfish.jersey.grizzly2.httpserver.GrizzlyHttpServerFactory;
+import org.glassfish.jersey.server.ResourceConfig;
+import org.junit.jupiter.api.AfterAll;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import utils.EMF_Creator;
+
+import javax.persistence.EntityManager;
+import javax.persistence.EntityManagerFactory;
+import javax.ws.rs.core.UriBuilder;
+import java.net.URI;
+import java.util.ArrayList;
+import java.util.List;
+
+import static io.restassured.RestAssured.given;
+import static junit.framework.Assert.assertEquals;
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.Matchers.containsInAnyOrder;
+import static org.hamcrest.Matchers.equalTo;
+
+public class RentalResourceTest {
+
+    private static final int SERVER_PORT = 7777;
+    private static final String SERVER_URL = "http://localhost/api";
+    private static Rental t1, t2;
+    private static final Gson GSON = new GsonBuilder().setPrettyPrinting().create();
+    List<Tenant> tenants = new ArrayList<>();
+    House house = new House(1,"address2","testby2",30);
+
+    static final URI BASE_URI = UriBuilder.fromUri(SERVER_URL).port(SERVER_PORT).build();
+    private static HttpServer httpServer;
+    private static EntityManagerFactory emf;
+
+    static HttpServer startServer() {
+        ResourceConfig rc = ResourceConfig.forApplication(new ApplicationConfig());
+        return GrizzlyHttpServerFactory.createHttpServer(BASE_URI, rc);
+    }
+
+    @BeforeAll
+    public static void setUpClass() {
+        //This method must be called before you request the EntityManagerFactory
+        EMF_Creator.startREST_TestWithDB();
+        emf = EMF_Creator.createEntityManagerFactoryForTest();
+
+        httpServer = startServer();
+        //Setup RestAssured
+        RestAssured.baseURI = SERVER_URL;
+        RestAssured.port = SERVER_PORT;
+        RestAssured.defaultParser = Parser.JSON;
+    }
+
+    @AfterAll
+    public static void closeTestServer() {
+        //System.in.read();
+
+        //Don't forget this, if you called its counterpart in @BeforeAll
+        EMF_Creator.endREST_TestWithDB();
+        httpServer.shutdownNow();
+    }
+
+    // Setup the DataBase (used by the test-server and this test) in a known state BEFORE EACH TEST
+    //TODO -- Make sure to change the EntityClass used below to use YOUR OWN (renamed) Entity class
+    @BeforeEach
+    public void setUp() {
+        EntityManager em = emf.createEntityManager();
+        t1 = new Rental("2222-02-02","2222-22-22",2222,2222,house);
+        t2 = new Rental("1111-11-11","1111-11-11",1111,1111,house);
+
+
+        try {
+            em.getTransaction().begin();
+            em.createQuery("delete from Rental ").executeUpdate();
+            em.persist(t1);
+            em.persist(t2);
+            em.getTransaction().commit();
+        } finally {
+            em.close();
+        }
+    }
+
+    @Test
+    public void testServerIsUp() {
+        System.out.println("Testing is server UP");
+        given().when().get("/rental/all").then().statusCode(200);
+    }
+
+    @Test
+    public void testLogRequest() {
+        System.out.println("Testing logging request details");
+        given().log().all()
+                .when().get("/rental/all")
+                .then().statusCode(200);
+    }
+
+    @Test
+    public void testLogResponse() {
+        System.out.println("Testing logging response details");
+        given()
+                .when().get("/rental/all")
+                .then().log().body().statusCode(200);
+    }
+
+    @Test
+    public void testGetById()  {
+        given()
+                .contentType(ContentType.JSON)
+                .get("/rental/{id}",t1.getId())
+                .then()
+                .assertThat()
+                .statusCode(HttpStatus.OK_200.getStatusCode())
+                .body("id", equalTo(t1.getId()))
+                .body("endDate", equalTo(t1.getEndDate()));
+    }
+
+    @Test
+    public void testError() {
+        given()
+                .contentType(ContentType.JSON)
+                // .pathParam("id", u1.getId()).when()
+                .get("/rental/"+20)
+                .then()
+                .assertThat()
+                .statusCode(HttpStatus.NOT_FOUND_404.getStatusCode())
+                .body("code", equalTo(404))
+                .body("message", equalTo("There's no rental with that id"));
+    }
+
+
+
+    @Test
+    public void testPrintResponse(){
+        Response response = given().when().get("/rental/"+t1.getId());
+        ResponseBody body = response.getBody();
+        System.out.println(body.prettyPrint());
+
+        response
+                .then()
+                .assertThat()
+                .body("endDate",equalTo("2222-22-22"));
+    }
+
+
+    @Test
+    public void exampleJsonPathTest() {
+        Response res = given().get("rental/"+t1.getId());
+        assertEquals(200, res.getStatusCode());
+        String json = res.asString();
+        JsonPath jsonPath = new JsonPath(json);
+        assertEquals("2222-22-22", jsonPath.get("endDate"));
+    }
+
+    @Test
+    void deleteRental() {
+        given()
+                .contentType("application/json")
+                .pathParam("id", t2.getId())
+                .delete("rental/{id}")
+                .then()
+                .statusCode(200)
+                .body("id", equalTo(t2.getId()));
+    }
+
+
+
+/*
+    @Test
+    public void getAllUsers() throws Exception {
+        List<Rental> tripDTOs;
+        tripDTOs = given()
+                .contentType("application/json")
+                .when()
+                .get("/trip/all")
+                .then()
+                .extract().body().jsonPath().getList("", Trip.class);
+        TripDTO t1DTO = new TripDTO(t1);
+        TripDTO t2DTO = new TripDTO(t1);
+        assertThat(tripDTOs, containsInAnyOrder(t1DTO, t2DTO));
+    }
+    */
+}
